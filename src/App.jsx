51b7267c6fc214a0383bd35d8f236d5b5b4f6ab1
hamdan.ga <hamdanchitari@gmail.com>
2025-02-04 19:46:11 +0300

    pet added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 // src/App.jsx
+import { useState, useEffect } from 'react';
+import * as petService from './services/petService';
+import PetList from './components/PetList/PetList';
+import PetDetail from './components/PetDetail/PetDetail';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [pets, setPets] = useState([]);
+
+  const [selected, setSelected] = useState(null);
+
+  const handleSelect = (pet) => {
+    setSelected(pet)
+  }
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const fetchedPets = await petService.index();
+        console.log(fetchedPets)
+        if (fetchedPets.err) {
+          throw new Error(fetchedPets.err);
+        }
+        setPets(fetchedPets);
+      } catch (err) {
+        
+        console.log(err);
+      }
+    };
+    fetchPets();
+  }, []);
+  return (
+    <>
+      <PetList pets={pets} handleSelect={handleSelect}/>
+      <PetDetail selected={selected} />
+    </>
+  );
 };
 
 export default App;
